143320f95d29bb755f1f0e2e97fac474651d7b7c
luxuan10 <luxuan10@meituan.com>
2026-03-26 13:46:07 +0800

    embedding

---
+++ cs336_basics/embedding.py
@@ -0,0 +1,17 @@
+
+import torch.nn as nn
+import torch
+
+class Embedding(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim: int, device=None, dtype=None):
+        super(Embedding, self).__init__()
+        self.num_embeddings = num_embeddings;
+        self.embedding_dim =  embedding_dim;
+        # 没必要这样初始化
+        self.weights = nn.Parameter(torch.empty(num_embeddings, embedding_dim, device=device, dtype=dtype))
+        # self.weights = nn.Parameter(torch.randn(num_embeddings, embedding_dim, device=device, dtype=dtype))
+        std = (2.0 / (num_embeddings + embedding_dim)) ** 0.5
+        torch.nn.init.trunc_normal_(self.weights, mean=0.0, std=std, a=-3.0*std, b= 3.0*std)
+
+    def forward(self, token_ids: torch.Tensor):
+        return self.weights[token_ids]
--- tests/adapters.py
+++ tests/adapters.py
@@ -11,6 +11,7 @@ from torch import Tensor
 
 from cs336_basics.tokenizer import Tokenizer, train_bpe
 from cs336_basics.linear import Linear
+from cs336_basics.embedding import Embedding
 
 
 def run_linear(
@@ -55,8 +56,10 @@ def run_embedding(
     Returns:
         Float[Tensor, "... d_model"]: Batch of embeddings returned by your Embedding layer.
     """
+    embd = Embedding(vocab_size, d_model)
+    embd.weights = torch.nn.Parameter(weights)
+    return embd.forward(token_ids=token_ids)
 
-    raise NotImplementedError
 
 
 def run_swiglu(
